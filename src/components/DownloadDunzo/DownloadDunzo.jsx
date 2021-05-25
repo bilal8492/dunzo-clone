d7581b4935176d0343c66a65fcccbe863bfb9e7d
Ansari Bilal <m.bilal8492@gmail.com>
2021-05-25 18:55:22 +0530

    add dunzo link

--- src/components/DownloadDunzo/DownloadDunzo.jsx
+++ src/components/DownloadDunzo/DownloadDunzo.jsx
@@ -5,8 +5,21 @@ export const DownloadDunzo = () => {
     return (
         <div className="dunzo-container">
             <div className="dunzo-link">
-                <img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/dunzo-app-be5ce8c58d3ad0b183757f34179879b4.png" alt="app" title="app" width="176px" height="240px" class="sc-1go0t46-0 echbQn" />
-
+                <div className="phone">
+                    <img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/dunzo-app-be5ce8c58d3ad0b183757f34179879b4.png" alt="app" title="app" width="176px" height="240px" class="sc-1go0t46-0 echbQn" />
+                </div>
+                <div className="app-link">
+                    <h4 className="irbvCt">All this from the convenience of your phone.
+                    <p className="kdbvtR"> Download the Dunzo mobile app. </p></h4>
+                    <div className="and-app">
+                        <div className="android">
+                            <a href="https://play.google.com/store/apps/details?id=com.dunzo.user&amp;hl=en" target="_blank" rel="noreferrer" class="jxbqi7-0 sc-6t3ece-3 eXwMAS"><img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/playstore-fe053d8036d653fed3955cd2c2a1e7e2.svg" alt="" title="" width="inherit" height="100%" class="sc-1go0t46-0 lirvZM sc-6t3ece-4 hSUXjV" /></a>
+                        </div>
+                        <div className="apple">
+                            <a href="https://itunes.apple.com/in/app/dunzoit/id1032391728?mt=8" target="_blank" rel="noreferrer" class="jxbqi7-0 sc-6t3ece-3 eXwMAS"><img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/appstore-43cd8d3a00a6ed32c485951de9b3af63.svg" alt="" title="" width="inherit" height="100%" class="sc-1go0t46-0 lirvZM sc-6t3ece-4 hSUXjV" /></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/SmallCardContainer/SmallCardContainer.jsx
+++ src/components/SmallCardContainer/SmallCardContainer.jsx
@@ -7,17 +7,17 @@ const SmallCardContainer = () => {
         <div className="small-container">
             <SmallCard
                 imgUrl="https://ik.imagekit.io/dunzo/home/tr:w-80,h-80_home_icon/icons/R4_Icons/Home/default_meat_tertiary_grid_1609739765849.png"
-                title="Hello"
+                title="Meat and Fish"
                 altText="img"
             />
 
             <SmallCard imgUrl="https://ik.imagekit.io/dunzo/home/tr:w-80,h-80_home_icon/icons/R4_Icons/Home/default_pets_tertiary_grid_1609739755734.png"
-                altText="Imag" title="Meat and Fish" />
+                altText="Imag" title="Pet Supplies" />
             <SmallCard imgUrl="https://ik.imagekit.io/dunzo/home/tr:w-80,h-80_home_icon/icons/R4_Icons/Home/paan_grid_1609739855940.png"
-                altText="img" title="Meat and Fish" />
+                altText="img" title="Paan Shop" />
             <SmallCard className="small-card"
                 imgUrl="https://ik.imagekit.io/dunzo/home/tr:w-80,h-80_home_icon/icons/R4_Icons/Home/default_others_tertiary_grid_1609739745871.png"
-                title="Meat and Fish" altText="img" />
+                title="Any store in the City" altText="img" />
         </div>
     )
 }
